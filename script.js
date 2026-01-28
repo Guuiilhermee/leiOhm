e8b46a14afc7ac8cc8665c3c819d7a16d8b8e6b6
GuilhermeA <guiilherme.ambrosio@gmail.com>
2026-01-28 16:08:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function calcLeiOhm(resistencia, corrente){
+    return resistencia * corrente
+}
+
+let resposta = document.getElementById('resposta')
+let calcular = document.getElementById('calcular')
+
+calcular.addEventListener('click' , ()=>{
+
+    let resistencia = Number(document.getElementById('resistencia').value)
+    let corrente = Number(document.getElementById('corrente').value)
+
+    volts = calcLeiOhm(resistencia, corrente)
+    
+    resposta.innerHTML = ``
+    resposta.innerHTML += `Volts: <b>${volts.toFixed(2)}</b>`
+})
